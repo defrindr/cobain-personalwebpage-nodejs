2d698dc40acf02558124da2765e1bc1a63562f78
Defri Indra Mahardika <defrindr@gmail.com>
2021-07-05 17:41:29 +0700

    .

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+// create an express app
+const express = require("express")
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
