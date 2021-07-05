23393dc8923f67243361e957a5071326e695bb59
Defri Indra Mahardika <defrindr@gmail.com>
2021-07-05 17:53:49 +0700

    .

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const app = express()
 // use the express-static middleware
 app.use(express.static("public"))
 
+//set engine
+app.set('view engine', 'hbs')
+
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+  res.render("pages/index.hbs");
 })
 
 // start the server listening for requests
